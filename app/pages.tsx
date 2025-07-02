c11324333584204cfa46761f8bcb586442df402e
workabert <21-69059@g.batstate-u.edu.ph>
2025-07-02 16:16:16 +0800

    Create pages.tsx

---
+++ app/pages.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <h1 className="text-4xl font-bold text-blue-600">Robert's Facebook is now working ðŸŽ‰</h1>
+    </main>
+  );
+}
